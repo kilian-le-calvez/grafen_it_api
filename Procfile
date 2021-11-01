14c26262fed08f1bf28081164d878e7d8eb1e488
Kilian Le Calvez <kilian.le-calvez@epitech.eu>
2021-11-01 15:53:21 +0100

    [+] env config

---
+++ Procfile
@@ -0,0 +1,5 @@
+release: python mangage.py makemigrations --no-input
+
+release: python mangage.py migrate --no-input
+
+web: gunicorn backend.wsgi
--- backend/settings.py
+++ backend/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -137,3 +135,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+django_heroku.settings(locals())
