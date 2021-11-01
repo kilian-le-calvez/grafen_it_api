815f8a7c8b61320916353beca4e3a02db48a447c
Kilian Le Calvez <kilian.le-calvez@epitech.eu>
2021-11-01 17:25:41 +0100

    [+] video field

--- videos/urls.py
+++ videos/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('videos/<str:pk>/delete/', views.deleteVideo),
     path('videos/<str:pk>/', views.getVideo),
 ] 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_URL)
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
