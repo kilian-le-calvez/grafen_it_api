e3e52f49c0abefcf70689470dd1f41a4865cf6e4
Kilian Le Calvez <kilian.le-calvez@epitech.eu>
2021-11-04 17:01:42 +0100

    [+] update request parameters and url

--- questions/urls.py
+++ questions/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('videos/<str:pk>/questions', views.getQuestions),
-    path('videos/<str:pk>/questions/create', views.createQuestion),
-    path('videos/<str:pk>/questions/delete', views.deleteQuestion),
+    path('videos/<str:id_video>/questions', views.getQuestions),
+    path('videos/<str:id_video>/questions/create', views.createQuestion),
+    path('videos/<str:id_video>/questions/delete/<str:id_question>', views.deleteQuestion),
 ]
--- questions/views.py
+++ questions/views.py
@@ -4,20 +4,20 @@ from .serializers import QuestionSerializer
 from .models import Question
 
 @api_view(['GET'])
-def getQuestions(request):
+def getQuestions(request, id_video):
     questions = Question.objects.all()
     serializer = QuestionSerializer(questions, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
-def createQuestion(request):
+def createQuestion(request, id_video):
     data = request.data
     question = Question.objects.create(question=data['question'], author=data['author'])
     serializer = QuestionSerializer(question, many=False)
     return Response(serializer.data)
 
 @api_view(['DELETE'])
-def deleteQuestion(request, pk):
-    question = Question.objects.get(id=pk)
+def deleteQuestion(request, id_video, id_question):
+    question = Question.objects.get(id=id_question)
     question.delete()
     return Response('Question was deleted')
