e21a33d84cb948d6545e829cf25c6dcf9fd81d86
Kilian Le Calvez <kilian.le-calvez@epitech.eu>
2021-11-04 15:44:44 +0100

    [FIX] name routes api

--- videos/urls.py
+++ videos/urls.py
@@ -5,11 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('videos/', views.getVideos),
+    path('videos', views.getVideos),
     path('videos/create', views.createVideo),
-    path('videos/<str:pk>/update/', views.updateVideo),
-    path('videos/<str:pk>/delete/', views.deleteVideo),
-    path('videos/<str:pk>/', views.getVideo),
+    path('videos/<str:pk>/update', views.updateVideo),
+    path('videos/<str:pk>/delete', views.deleteVideo),
+    path('videos/<str:pk>', views.getVideo),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
