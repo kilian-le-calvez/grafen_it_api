ea63bdfe1a49c57aeaa4e6403e29d004931e41e5
Kilian Le Calvez <kilian.le-calvez@epitech.eu>
2021-11-04 16:54:23 +0100

    [+] add Question to admin

--- backend/urls.py
+++ backend/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.getRoutes),
+    path('admin/', admin.site.urls),
     path('', include('videos.urls')),
     path('', include('questions.urls')),
 ]
