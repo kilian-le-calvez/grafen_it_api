1f8be28efbd70c52d9e4888c546663dbb66142b2
Kilian Le Calvez <kilian.le-calvez@epitech.eu>
2021-11-05 11:51:40 +0100

    [+] update urls routes

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,23 @@
+import sys
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
+sys.path.append('..')
+from questions import views
+from videos import views
 
 urlpatterns = [
     path('', views.getRoutes),
     path('admin/', admin.site.urls),
-    path('videos/', include('videos.urls')),
-    path('questions/', include('questions.urls')),
+    path('videos/', views.getVideos),
+    path('videos/create/', views.createVideo),
+    path('videos/delete/', views.deleteVideo),
+    path('questions/', views.getQuestions),
+    path('questions/create/', views.createQuestion),
+    path('questions/delete/', views.deleteQuestion),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- questions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.getQuestions),
-    path('create/', views.createQuestion),
-    path('delete/', views.deleteQuestion),
-]
